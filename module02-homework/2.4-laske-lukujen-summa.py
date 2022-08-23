21efeb4b4101218c82025964d3a0c16117012614
kassu11 <61124141+kassu11@users.noreply.github.com>
2022-08-23 16:13:12 +0300

    Update 2.4-laske-lukujen-summa.py

--- module02-homework/2.4-laske-lukujen-summa.py
+++ module02-homework/2.4-laske-lukujen-summa.py
@@ -3,3 +3,5 @@ lukub = int(input("Korjoita toka luku: "))
 lukuc = int(input("Korjoita kolmas luku: "))
 
 print("lukujen summa on:", lukua + lukub + lukuc)
+print("lukujen tulo on:", lukua * lukub * lukuc)
+print("lukujen keskiarvo on:", (lukua + lukub + lukuc) / 3)
