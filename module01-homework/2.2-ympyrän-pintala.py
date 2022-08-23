c7e08d2b4928b3a6c67566b23c56e888f32a454a
kassu11 <61124141+kassu11@users.noreply.github.com>
2022-08-23 15:22:52 +0300

    2.2 ja esimerkkien parannukset

--- module01-homework/2.2-ympyrän-pintala.py
+++ module01-homework/2.2-ympyrän-pintala.py
@@ -1,5 +1,5 @@
 import math
 
 print("Lasken ympyrän pinta-alan")
-sade = int(input("Kerro ympyrän säde?: "))
-print("Ympyrän pinta-ala on: ", math.pi * sade ** 2)
+sade = int(input("Kerro ympyrän säde metreinä?: "))
+print(f"{sade} (m) säteisen ympyrän pinta-ala on: {(math.pi * sade ** 2):.3f} neliömetriä")
--- module02-examples/esimerkit-v2.py
+++ module02-examples/esimerkit-v2.py
@@ -1,4 +1,15 @@
+import math
+import random
+
 arvo = 1/3
 
 print(f"Tulosta arvo {arvo:.2f}")
 print(f"Tulosta arvo {arvo:.5f}")
+
+r = 2.5
+area = math.pi * r * r
+print(f"{r} (m) säteisen pinta-ala on {area} neliömetriä")
+
+
+
+print(random.randint(10, 20))
